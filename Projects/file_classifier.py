c59bb5a2323b6b587e2900b6e8b51522cad473a6
Pavani Pothuganti <pavanipothuganti58@gmail.com>
2026-02-11 11:10:31 +0000

    new file

---
+++ Projects/file_classifier.py
@@ -0,0 +1,21 @@
+def classify_files(gl, project_id: int) -> dict:
+    """
+    Classifies files by type.
+    """
+
+    try:
+        project = gl.projects.get(project_id)
+        files = project.repository_tree(path="", recursive=True)
+
+        counts = {}
+
+        for f in files:
+            name = f["name"]
+            if "." in name:
+                ext = name.split(".")[-1]
+                counts[ext] = counts.get(ext, 0) + 1
+
+        return counts
+
+    except Exception as e:
+        return {"error": str(e)}
